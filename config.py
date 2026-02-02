c155f914da74aa14e2c002a9104351eccd8a60e0
robDevs <robbdevv@gmail.com>
2026-02-01 21:27:33 -0700

    config

---
+++ config.py
@@ -0,0 +1,10 @@
+SERIAL_PORT = "/dev/ttyUSB1" # probably shouldn't hard code this
+PRIVATE_CHANNEL_NAME = "Home"
+NODE_NAME = "mesh-bot"
+
+STATE_FILE = "bot_state.json" #will be used for saving the state eventually. 
+
+ADMIN_NODES = {
+    #tbd
+    #will be used for admin commands eventually. EG: reboot or something like that. 
+}
